510a6fc75418715f1300ee9edb65ed2ab10e28a8
panadolextra91 <126870223+panadolextra91@users.noreply.github.com>
2025-04-01 05:18:52 +0700

    config base api into vercel be url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    API_BASE_URL: 'http://localhost:3000/api'
+    //API_BASE_URL: 'http://localhost:3000/api'
+    API_BASE_URL: 'https://medimaster-be.vercel.app/api'
 };
 
 export default config;
